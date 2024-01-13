d0484a1db9f5e76c15977ae1f9cd5979aa39ca8d
vinayak-naik <vinayaknaik2021@gmail.com>
2024-01-13 22:55:05 +0530

    minor changes

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-      react(),
-      federation({
-        name: 'container',
-        remotes: {
-          layoutMfe: "http://localhost:5001/assets/remoteEntry.js",
-          authMfe: "http://localhost:5002/assets/remoteEntry.js",
-          formbuilderMfe: "http://localhost:5003/assets/remoteEntry.js",
-          // formbuilderMfe: "https://ecom-layout-mfe.vercel.app/assets/remoteEntry.js",
-          // authMfe: "https://ecom-auth-mfe.vercel.app/assets/remoteEntry.js",
-        },
-        shared: ['react','react-dom']
-      })
+    react(),
+    federation({
+      name: "container",
+      remotes: {
+        layoutMfe: "http://localhost:5001/assets/remoteEntry.js",
+        authMfe: "http://localhost:5002/assets/remoteEntry.js",
+        userMfe: "http://localhost:5003/assets/remoteEntry.js",
+        formbuilderMfe: "http://localhost:5004/assets/remoteEntry.js",
+      },
+      shared: ["react", "react-dom"],
+    }),
   ],
   build: {
     modulePreload: false,
@@ -24,4 +23,4 @@ export default defineConfig({
     minify: false,
     cssCodeSplit: false,
   },
-})
+});
