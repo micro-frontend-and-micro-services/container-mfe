851bc6748179e1b550d8dc4c6e44c7ed0f567e75
vinayak-naik <vinayaknaik2021@gmail.com>
2024-01-18 11:34:03 +0530

    minor

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 
-const prod=true
+const prod=false
 
 const local = {
   layoutMfe: "http://localhost:5001/assets/remoteEntry.js",
